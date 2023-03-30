96f97deeda1c7c98ca3617b06c2d70176a1deab4
Filipe Melo <filipe.silva@encora.com>
2023-03-30 17:24:52 -0300

    install prisma

---
+++ prisma/migrations/20230330202240_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "phone" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
