2f3f04dbbed6ad5d055e4fb05059cf3907b9d506
Filipe Melo <filipe.silva@encora.com>
2023-04-01 19:06:32 -0300

    feature: create Customer setter methods

--- src/app/entities/customer.ts
+++ src/app/entities/customer.ts
@@ -20,19 +20,38 @@ export class Customer {
     return this._id;
   }
 
+  public set id(id: string) {
+    this._id = id;
+  }
   public get email() {
     return this.props.email;
+  }
+
+  public set email(email: string) {
+    this.props.email = email;
   }
 
   public get name() {
     return this.props.name;
   }
 
+  public set name(name: string) {
+    this.props.name = name;
+  }
+
   public get phone() {
     return this.props.phone;
   }
 
+  public set phone(phone: string) {
+    this.props.phone = phone;
+  }
+
   public get password() {
     return this.props.password;
+  }
+
+  public set password(password: string) {
+    this.props.password = password;
   }
 }
