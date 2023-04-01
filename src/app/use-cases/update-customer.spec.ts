646a834186f96d821b180a3c3ca811d99113b185
Filipe Melo <filipe.silva@encora.com>
2023-04-01 19:31:22 -0300

    chore: fix update customer test-case

--- src/app/use-cases/update-customer.spec.ts
+++ src/app/use-cases/update-customer.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryCustomerRepository } from '@test/repositories/in-memory-customer-repository';
 import { UpdateCustomer } from './update-customer';
 import { makeCustomer } from '@test/factories/customer-factory';
+import { CustomerNotFound } from './errors/customer-not-found';
 
 describe('Update Customer', () => {
   it("should update the customer's insformation", async () => {
@@ -15,5 +16,14 @@ describe('Update Customer', () => {
     });
 
     expect(customerRespository.customers[0].name).toEqual('filipe melo');
+  });
+
+  it("should not update a customer's information", () => {
+    const customerRepository = new InMemoryCustomerRepository();
+    const updateCustomerService = new UpdateCustomer(customerRepository);
+
+    expect(() => {
+      return updateCustomerService.execute({ id: 'whatever-non-existing-id' });
+    }).rejects.toThrow(CustomerNotFound);
   });
 });
