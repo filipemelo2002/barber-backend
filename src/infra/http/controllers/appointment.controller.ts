babffc78c9f270f0cf09a02cbd853525082a6e4b
Filipe Melo <filipe.silva@encora.com>
2023-04-15 12:16:28 -0300

    feature: protect appointment endpoints

--- src/infra/http/controllers/appointment.controller.ts
+++ src/infra/http/controllers/appointment.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateAppointmentBody } from '../dtos/create-appointment-body';
 import { CreateAppointment } from '@app/use-cases/create-appointment';
 import { AppointmentViewModel } from '../view-model/appointment-view-model';
 import { ListAppointmentsByDay } from '@app/use-cases/list-appointments-by-day';
 import { ListAppointmentsByCustomerAndDay } from '@app/use-cases/list-appointments-by-customer-and-day';
+import { AdminCustomerGuard } from '@infra/auth/admin-customer.guard';
 
 @Controller('appointments')
 export class AppointmentController {
@@ -13,6 +22,7 @@ export class AppointmentController {
     private listAppointmentByCustomerAndDay: ListAppointmentsByCustomerAndDay,
   ) {}
   @Post()
+  @UseGuards(AdminCustomerGuard)
   async create(@Body() body: CreateAppointmentBody) {
     const { customerId, dueDate } = body;
     const { appointment } = await this.createAppointment.execute({
@@ -26,6 +36,7 @@ export class AppointmentController {
   }
 
   @Get()
+  @UseGuards(AdminCustomerGuard)
   async getByDay(@Query('dueDate') dueDate: string) {
     const { appointments } = await this.listAppointmentByDay.execute({
       dueDate: new Date(dueDate),
@@ -37,6 +48,7 @@ export class AppointmentController {
   }
 
   @Get('/customer/:id')
+  @UseGuards(AdminCustomerGuard)
   async getByUserAndDay(
     @Param('id') id: string,
     @Query('dueDate') dueDate?: string,
